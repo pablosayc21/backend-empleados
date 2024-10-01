74de96e6ed3b5d6e2fe5ff31d4eb65c23183a0bd
pablosayc21 <19001434@galileo.edu>
2024-09-30 18:43:42 -0600

    Script para crear la base de datos

---
+++ creacion_bd.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+conn = sqlite3.connect('database.db')
+
+
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS empleados")
+
+cursor.execute("DROP TABLE IF EXISTS departamentos")
+
+cursor.execute("""
+               
+    CREATE TABLE departamentos 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nombre TEXT NOT NULL)
+               
+""")
+
+cursor.execute("""
+               
+    CREATE TABLE empleados 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL,
+    departamento_id INTEGER NOT NULL,
+    fecha_contratacion DATE NOT NULL,
+    cargo TEXT NOT NULL,
+    FOREIGN KEY (departamento_id) REFERENCES departamentos (id))
+               
+""")
+
+conn.commit()
+
+conn.close()
+
+print("Creación de base de datos exitosa.")
